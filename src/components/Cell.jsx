7166f77d6f9709c117db276b2c5172413dc766e2
Parker <brownemac4@icloud.com>
2025-07-16 04:21:55 -0500

    sessionStorage and input for grid. Also did new game button

--- src/components/Cell.jsx
+++ src/components/Cell.jsx
@@ -1,11 +1,36 @@
 import { Form } from 'react-bootstrap'
+import { useState } from 'react'
 export default function Cell(props) {
 
+    const [value, setValue] = useState(sessionStorage.getItem("gridValues") ? JSON.parse(sessionStorage.getItem("gridValues"))[(props.row * 9) + props.col] : "");
+
     // <p>{props.row}{props.col}</p>
+    function changeGrid(val) {
+        let grid = [];
+        
+        if (sessionStorage.getItem("gridValues") && sessionStorage.getItem("gridValues") !== "0") {
+            const oldGrid = JSON.parse(sessionStorage.getItem("gridValues"));
+            for (let cell of oldGrid) {
+                grid.push(cell);
+            }
+        } else {
+
+            for (let i = 0; i < 81; i++) {
+                grid.push(0);
+            }
+
+        }
+        grid[ (props.row * 9) + props.col] = val;
+
+        sessionStorage.setItem("gridValues", JSON.stringify(grid));
+        // console.log("val: " + val + " grid " + grid);
+        setValue(val);
+        
+    }
 
     return <>
         <Form>
-            <Form.Control></Form.Control>
+            <Form.Control onChange={(input) => changeGrid(input.target.value)} placeholder={sessionStorage.getItem("gridValues") ? (value === 0 ? "": value) : ""} ></Form.Control>
         </Form>
     </>
 }
--- src/components/Game.jsx
+++ src/components/Game.jsx
@@ -1,16 +1,18 @@
-import { Container, Row, Col } from "react-bootstrap"
+import { Container, Row, Col, Button } from "react-bootstrap"
 import { Navbar, Card } from 'react-bootstrap';
 import Cell from './Cell'
+import { useState } from 'react'
 
 export default function Game() {
 
     const nums = [0, 1, 2];
 
+
+
     // create a 3x3 grid of Cards with the Cards containing a 3x3 grid of Cells
     // The Cell in the top left has index 00 and the Cell in the bottom right has index 88.
     // For readability the three outside rows are created by copy and pasting instead of mapping. 
     return <>
-        
         <Container>  
             <Card>
                 <Row>
@@ -20,7 +22,7 @@ export default function Game() {
                                         <Container>
                                             {
                                                 nums.map((row) => <Row key={row}> {
-                                                    nums.map((col) => <Col key={col}><Cell row={row} col={col + square}></Cell></Col>)
+                                                    nums.map((col) => <Col key={col}><Cell row={row} col={col + (square * 3)}></Cell></Col>)
                                                 }</Row>)
                                             }
                                         </Container>
@@ -35,7 +37,7 @@ export default function Game() {
                                         <Container>
                                             {
                                                 nums.map((row) => <Row key={row}> {
-                                                    nums.map((col) => <Col key={col}><Cell row={row + 3} col={col + square}></Cell></Col>)
+                                                    nums.map((col) => <Col key={col}><Cell row={row + 3} col={col + (square * 3)}></Cell></Col>)
                                                 }</Row>)
                                             }
                                         </Container>
@@ -50,7 +52,7 @@ export default function Game() {
                                         <Container>
                                             {
                                                 nums.map((row) => <Row key={row}> {
-                                                    nums.map((col) => <Col key={col}><Cell row={row + 6} col={col + square}></Cell></Col>)
+                                                    nums.map((col) => <Col key={col}><Cell row={row + 6} col={col + (square * 3)}></Cell></Col>)
                                                 }</Row>)
                                             }
                                         </Container>
@@ -60,6 +62,18 @@ export default function Game() {
                 </Row>
             </Card>
         </Container>
+        <Container>
+            <Row>
+                <Col>
+                    <Button onClick={() => {
+                                sessionStorage.setItem("gridValues", 0)
+                            }
+                        }> New Game </Button>
+                    <Button > Set values </Button>
+                </Col>
+            </Row>
+        </Container>
+        
     </>
     
 }
