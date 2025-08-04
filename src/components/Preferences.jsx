849435d5cbc1938119399796a63faceb4031d278
Parker <brownemac4@icloud.com>
2025-08-03 23:49:12 -0500

    advanced strategies

--- src/components/Preferences.jsx
+++ src/components/Preferences.jsx
@@ -1,9 +1,43 @@
 import SudokuNavBar from "./SudokuNavBar"
+import styles from "../modes.module.css"
+import { useState } from "react"
+import { Col, Container, Row } from "react-bootstrap";
 
 export default function Preferences(props) {
+
+    // const [mode, setMode] = useState("lightmode");
+    const [textColor, setTextColor] = useState("black");
+
     return <>
-        <SudokuNavBar/>
-        <h1 style={{marginTop: 25}}>Preferences</h1>
-        <p>To be implemented</p>
+            <SudokuNavBar/>
+            <Container>
+                <h1 style={{marginTop: 25}}>Preferences</h1>
+                {/* <button onClick={() => setMode((prev) => mode === "lightmode" ? "darkmode" : "lightmode")}>{mode === "lightmode" ? "Turn on darkmode" : "Turn on light mode"}</button> */}
+                <p  style={{color: "" + textColor}}>To be implemented</p>
+            </Container>
+
+            <p>Text Color</p>
+            <Container>
+                <Row>
+                    <Col>
+                        <button onClick={() => setTextColor("red")}>Red</button>
+                    </Col>
+                    <Col>
+                        <button onClick={() => setTextColor("blue")}>Blue</button>
+                    </Col>
+                    <Col>
+                        <button onClick={() => setTextColor("green")}>Green</button>
+                    </Col>
+                    <Col>
+                        <button onClick={() => setTextColor("black")}>Black</button>
+                    </Col>
+                </Row>
+            </Container>
+
+            {/* <p>Paragraph text size</p>
+            <input></input>
+         */}
+        
+        
     </>
 }
