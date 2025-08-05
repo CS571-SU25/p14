3a190273f90a8ea8846d5902ca5a0ef39caca390
Parker <brownemac4@icloud.com>
2025-08-04 21:29:49 -0500

    formatted strategies page

--- src/components/stratComponents/Beginner.jsx
+++ src/components/stratComponents/Beginner.jsx
@@ -17,13 +17,15 @@ export default function Beginner(props) {
         <Container style={pStyle}>
             <Row>
                 <Col>
+                    <p>
+                        If a box, row, or column has all the numbers except for one filled out, then the uncompleted cell must be the number that the box, row, or column doesn't contain yet.
+                    </p>
+                </Col>
+                <Col>
                     <img src={lastCellInBox} width={300} alt="Image of a 3x3 box of cells. Each cell except for top left cell are filled in with a number 1-8. The top left cell has the number 9 written in as a note, but not filled in as a value for the cell."></img>
             
                 </Col>
                 <Col>
-                    <p>
-                        If a box, row, or column has all the numbers except for one filled out, then the uncompleted cell must be the number that the box, row, or column doesn't contain yet.
-                    </p>
                     <p>
                         For example, in the image to the left all the cells the box already has cells with the numbers 1-8 in it, so the box just needs the number nine. 
                         Therefore the only uncompleted cell (the top left cell) must be the number 9.
@@ -37,13 +39,16 @@ export default function Beginner(props) {
         <Container style={pStyle}>
             <Row>
                 <Col>
+                    <p>
+                        If a cell cannot be any of the numbers 1-9 except for one number then the value of the cell is the only number 1-9 that can fit in the cell.
+                    </p>
+                </Col>
+                <Col>
                     <img src={lastOptionInCell} width={300} alt="Image of an incomplete sudoku board. One cell on the board has a note marking the number 9. The column of that cell has cells with the numbers 1, 7, and 5 filled out. The row of that cell has the numbers 2, 4, and 8 filled out. The box of that cell has the numbers 3 and 6 filled out."></img>
             
                 </Col>
                 <Col>
-                    <p>
-                        If a cell cannot be any of the numbers 1-9 except for one number then the value of the cell is the only number 1-9 that can fit in the cell.
-                    </p>
+                    
                     <p>
                         For example, the cell in the picture noted with the number nine must be a nine because the column it's in already has 1, 7, and 5; the row that it's in already has 2, 4, and 8; and the box that it's in already has the numbers 3 and 6.
                         Because of that none of the numbers 1, 2, 3, 4, 5, 6, 7, or 8 can be in the cell, so the only possible option left is the number 9.
--- src/components/stratComponents/Intermediate.jsx
+++ src/components/stratComponents/Intermediate.jsx
@@ -17,14 +17,17 @@ export default function Intermediate(props) {
         <Container style={pStyle}>
             <Row>
                 <Col>
-                    <img src={hiddenPairs} width={300} alt="Image of a 3x3 box of cells. The cells in the bottom row and the middle right cell are filled out with numbers 4, 7, 8, and 9. The remaining cells have notes on what numbers they can be. While there doesn't seem to be a pattern about the numbers 1-3, the numbers 5 and 6 only reside in the two empty cells in the middle column."></img>
-                </Col>
-                <Col>
                     <p>
                         Hidden pairs are when n numbers each can only be in the same n cells in a box, row, or column. 
                         Because these n numbers can only fit in these n cells, no other numbers can fit in any of the n cells because if another value was placed in one of the cells there wouldn't be enough room for teh n numbers that need the n cells.
                         
                     </p>
+                </Col>
+                <Col>
+                    <img src={hiddenPairs} width={300} alt="Image of a 3x3 box of cells. The cells in the bottom row and the middle right cell are filled out with numbers 4, 7, 8, and 9. The remaining cells have notes on what numbers they can be. While there doesn't seem to be a pattern about the numbers 1-3, the numbers 5 and 6 only reside in the two empty cells in the middle column."></img>
+                </Col>
+                <Col>
+                    
                     <p>
                         For example, in the image to the left the only places in the box that the numbers 5 and 6 can go are in the two empty cells in the middle column.
                         The box needs both the numbers 5 and 6, so if the numbers 1, 2, or 3 filled one of those two cells then the box wouldn't be able to have all the numbers it needed and the solution wouldn't work.
@@ -39,15 +42,18 @@ export default function Intermediate(props) {
         <Container style={pStyle}>
             <Row>
                 <Col>
-                    <img src={nakedPairs} width={300} alt="Image of a 3x3 box of cells. The cells in the bottom row and the middle right cell are filled out with numbers 4, 7, 8, and 9. The remaining cells have notes on what numbers they can be. The remaining cells in the left and right columns have notes of some of the numbers 1, 2, 3, 5, or 6 with little to no pattern, however the empty cells in the middle row only have the numbers 5 and 6 in them."></img>
-            
-                </Col>
-                <Col>
                     <p>
                         If n amount of cells only have n possible numbers in a box, row, or column, then those n numbers can't be in any of the other cells in the box.
                         This is because if the n numbers are in other cells then there will only be n-1 numbers for n cells, which will leave a cell empty.
                         Therefore you can remove the n number from the other cells in the box, row, or column.
                     </p>
+                </Col>
+                <Col>
+                    <img src={nakedPairs} width={300} alt="Image of a 3x3 box of cells. The cells in the bottom row and the middle right cell are filled out with numbers 4, 7, 8, and 9. The remaining cells have notes on what numbers they can be. The remaining cells in the left and right columns have notes of some of the numbers 1, 2, 3, 5, or 6 with little to no pattern, however the empty cells in the middle row only have the numbers 5 and 6 in them."></img>
+            
+                </Col>
+                <Col>
+                    
                     <p>
                         For example, the two cells in the middle column only have the notes 5 and 6, so if the numbers 5 or 6 were used on one of the other cells in the box, one of the cells in the middle column wouldn't be able to be filled with a number.
                         Therefore, the two cells in the middle column are the only cells in the box that can contain a 5 or a 6 so we can remove the notes 5 and 6 from the other cells in the box.
